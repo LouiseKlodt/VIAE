09fb48d0a07df31aa4cea6ffa94238ac82ff96dc
LouiseKlodt <lklodt@gilt.com>
2018-09-06 08:27:03 +0100

    functional test passes

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,19 +1,25 @@
 import unittest
+from flask import Flask
+from flask_testing import TestCase
 from flask_testing import LiveServerTestCase
 from selenium import webdriver
-
-browser = webdriver.Safari()
-browser.get('http://localhost:5000')
-
-assert 'Django' not in browser.title
+import urllib
 
 
+class TestServerSetup(LiveServerTestCase):
 
-def create_app(self):
-        config_name = 'testing'
-        app = create_app(config_name)
-        app.config.update(
-            # Change the port that the liveserver listens on
-            LIVESERVER_PORT=8943
-        )
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
         return app
+    
+
+    def test_server_is_up_and_running(self):
+        response = urllib.request.urlopen(self.get_server_url())
+        self.assertEqual(response.code, 200)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
